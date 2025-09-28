18fa9542b6559ec0216082a9a338cad95623c91a
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-28 13:38:31 -0500

    Added the onMouseEnter to ProductGridItem

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,16 @@
-import { Title } from "@/components";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from '@/seed/seed';
+
+
+const products = initialData.products;
 
 
 export default function Home() {
   return (
     <>
       <Title title="Prueba" subtitle="Prueba subtitle" className="mb-2" />
+
+      <ProductGrid products={products} />
     </>
 
   );
