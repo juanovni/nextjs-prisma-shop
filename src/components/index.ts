ac237d98ca44386e45e7291643cd7852bce2e64c
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-28 13:21:54 -0500

    Added the PageNotFound componet

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
-// Archivos de barril
 
-export { TopMenu } from './ui/top-menu/TopMenu'
+// Archivos de barril
+export { PageNotFound } from './ui/not-found/PageNotFound';
+export { TopMenu } from './ui/top-menu/TopMenu';
