db907fbb3634b749dfd36aa216512588a6604f45
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-28 13:36:18 -0500

    Added the interfaces and ProductGrid

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,11 @@
 import { Title } from "@/components";
-import { titleFont } from "@/config/fonts";
+
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start justify-between p-24">
-      <Title title="Prueba" subtitle="Prueba subtitle"/>
-    </main>
+    <>
+      <Title title="Prueba" subtitle="Prueba subtitle" className="mb-2" />
+    </>
 
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,8 @@
 
+
 // Archivos de barril
 export { PageNotFound } from './ui/not-found/PageNotFound';
+export { ProductGrid } from './products/product-grid/ProductGrid';
+export { ProductGridItem } from './products/product-grid/ProductGridItem';
 export { Title } from './ui/title/Title';
 export { TopMenu } from './ui/top-menu/TopMenu';
