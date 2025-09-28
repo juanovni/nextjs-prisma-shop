3133ba5d8879a4bfe3bfeb1a146a78d787398508
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-28 12:02:21 -0500

    Applied Montserrat_Alternates fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start justify-between p-24">
       <h1>Hola Mundo</h1>
+      <h1 className={`${titleFont.className}`}>Hola Mundo!</h1>
     </main>
 
   );
