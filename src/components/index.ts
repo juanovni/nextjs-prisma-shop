dac49dfc5d87fda800452f1cfb7392fd469b7b5d
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-28 15:17:53 -0500

    Added the swiper and components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,9 @@
 
 
 
+
+
+
 // Archivos de barril
 export { PageNotFound } from './ui/not-found/PageNotFound';
 export { ProductGrid } from './products/product-grid/ProductGrid';
@@ -9,6 +12,7 @@ export { ProductGridItem } from './products/product-grid/ProductGridItem';
 export { Sidebar } from './ui/sidebar/Sidebar';
 export { Title } from './ui/title/Title';
 export { TopMenu } from './ui/top-menu/TopMenu';
-
-export * from './product/size-selector/SizeSelector';
 export * from './product/quantity-selector/QuantitySelector';
+export * from './product/size-selector/SizeSelector';
+export * from './product/slideshow/ProductMobileSlideshow';
+export * from './product/slideshow/ProductSlideshow';
