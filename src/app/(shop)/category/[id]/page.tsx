3c16acc041bb29d70be6d012311cdd295984f217
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-28 14:31:04 -0500

    Added products by category

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,42 @@
+'use client';
 
-export default function() {
+import React from 'react';
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products;
+
+
+interface Props {
+  params: Promise<{
+    id: Category;
+  }>
+}
+
+export default function CategoryPage({ params }: Props) {
+  const { id } = React.use(params);  // ✅ unwrap params
+
+  if (!['men', 'women', 'kid', 'unisex'].includes(id)) {
+    return <h1>Categoría no encontrada</h1>;
+  }
+
+  const productsByGender = products.filter(prod => prod.gender === id);
+
+  const labels: Record<Category, string> = {
+    'men': 'para hombres',
+    'women': 'para mujeres',
+    'kid': 'para ninos',
+    'unisex': 'para todos',
+  }
   return (
-    <div>
-      <h1>Category Page</h1>
-    </div>
+    <>
+      <Title
+        title={`Ropa - ${labels[id as Category]}`}
+        subtitle="Categorias"
+        className="mb-2" />
+
+      <ProductGrid products={productsByGender || []} />
+    </>
   );
 }
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,7 @@
 import { ProductGrid, Title } from "@/components";
 import { initialData } from '@/seed/seed';
 
-
 const products = initialData.products;
-
 
 export default function Home() {
   return (
