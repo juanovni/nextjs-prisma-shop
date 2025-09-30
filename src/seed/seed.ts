b9f7e7f119b311c7e8197f9e909ea1dc26dd8663
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-30 10:09:45 -0500

    Added the users in the seed

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,3 +1,5 @@
+import bcryptjs from "bcryptjs";
+
 interface SeedProduct {
     description: string;
     images: string[];
@@ -11,10 +13,18 @@ interface SeedProduct {
     gender: 'men' | 'women' | 'kid' | 'unisex';
 }
 
+interface SeedUser {
+    email: string;
+    password: string;
+    name: string;
+    role: 'admin' | 'user';
+}
+
 type ValidSizes = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
 type ValidTypes = 'shirts' | 'pants' | 'hoodies' | 'hats';
 
 interface SeedData {
+    users: SeedUser[];
     categories: string[];
     products: SeedProduct[];
 }
@@ -24,9 +34,25 @@ interface SeedData {
 
 export const initialData: SeedData = {
 
+    users: [
+        {
+            email: 'juan@gmail.com',
+            name: 'Juan Constantine',
+            role: 'admin',
+            password: bcryptjs.hashSync('123456')
+        },
+        {
+            email: 'jose@gmail.com',
+            name: 'Jose Murillo',
+            role: 'user',
+            password: bcryptjs.hashSync('123456')
+        }
+    ],
+
     categories: [
         'Shirts', 'Pants', 'Hoodies', 'Hats'
     ],
+
     products: [
         {
             description: "Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.",
