7edba95e06dcdd13420caa1b9004b125af75a2d4
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-30 09:53:41 -0500

    Added the loginForm to connected de server authjs

---
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import Link from "next/link"
+
+import { authenticate } from '@/actions';
+import { useActionState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { IoExpandOutline } from "react-icons/io5";
+
+export const LoginForm = () => {
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
+  const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined,
+  );
+
+  return (
+    <form action={formAction} className="flex flex-col">
+
+      <label htmlFor="email">Correo electrónico</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+        name="email"
+      />
+
+
+      <label htmlFor="password">Contraseña</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="password"
+        name="password"
+      />
+
+      <button
+
+        className="btn-primary">
+        Ingresar
+      </button>
+
+      <div
+        className="flex h-8 items-end space-x-1"
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        {errorMessage && (
+          <>
+            <IoExpandOutline className="h-5 w-5 text-red-500" />
+            <p className="text-sm text-red-500">{errorMessage}</p>
+          </>
+        )}
+      </div>
+      {/* divisor l ine */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-800">O</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+      <Link
+        href="/auth/new-account"
+        className="btn-secondary text-center">
+        Crear una nueva cuenta
+      </Link>
+
+    </form>
+  )
+}
