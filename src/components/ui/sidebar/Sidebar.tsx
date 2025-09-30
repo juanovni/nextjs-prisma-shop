b72963319effc47b3648e0e22e9addb65ab294ce
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-30 10:32:46 -0500

    Added the logout action

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -5,6 +5,7 @@ import clsx from 'clsx';
 import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from 'react-icons/io5';
 
 import { useUIStore } from '@/store';
+import { logout } from '@/actions';
 
 
 export const Sidebar = () => {
@@ -92,13 +93,13 @@ export const Sidebar = () => {
           <span className="ml-3 text-xl">Ingresar</span>
         </Link>
 
-        <Link
-          href="/"
-          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+        <button
+          onClick={() => logout()}
+          className="flex w-full items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
         >
           <IoLogOutOutline size={30} />
           <span className="ml-3 text-xl">Salir</span>
-        </Link>
+        </button>
 
         {/* Line Separator */}
         <div className="w-full h-px bg-gray-200 my-10" />
