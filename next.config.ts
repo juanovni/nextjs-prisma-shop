566645b9c5bb274deb6f89809d0355b006ad26ee
vista-juan-constantine <juan.constantine@vistadash.com>
2025-10-02 20:28:30 -0500

    Added the ProductsInCart to check page

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compiler: {
+    styledComponents: true
+  },
   webpack: (config) => {
     // Evita que Next intente empaquetar carpetas innecesarias
     config.externals.push({ prisma: 'commonjs prisma' });
--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { Title } from '@/components';
 import { initialData } from '@/seed/seed';
 import Image from 'next/image';
+import { ProductsInCart } from './ui/ProductsInCart';
 
 
 const productsInCart = [
@@ -26,44 +27,10 @@ export default function CheckoutPage() {
           {/* Carrito */}
           <div className="flex flex-col mt-5">
             <span className="text-xl">Ajustar elementos</span>
-            <Link href="/cart" className="underline mb-5">
-              Editar carrito
-            </Link>
 
+            <ProductsInCart />
 
-
-            {/* Items */}
-            {
-              productsInCart.map(product => (
-
-                <div key={product.slug} className="flex mb-5">
-                  <Image
-                    src={`/products/${product.images[0]}`}
-                    width={100}
-                    height={100}
-                    style={{
-                      width: '100px',
-                      height: '100px'
-                    }}
-                    alt={product.title}
-                    className="mr-5 rounded"
-                  />
-
-                  <div>
-                    <p>{product.title}</p>
-                    <p>${product.price} x 3</p>
-                    <p className="font-bold">Subtotal: ${product.price * 3}</p>
-                  </div>
-
-                </div>
-
-
-              ))
-            }
           </div>
-
-
-
 
           {/* Checkout - Resumen de orden */}
           <div className="bg-white rounded-xl shadow-xl p-7">
