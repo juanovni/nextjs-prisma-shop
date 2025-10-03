a3a50ae319e4d088461c198ae7dea9ff5597d31c
vista-juan-constantine <juan.constantine@vistadash.com>
2025-10-02 20:50:24 -0500

    Added the placeOrder

---
+++ src/app/(shop)/checkout/(checkout)/ui/PlaceOrder.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { useAddressStore, useCartStore } from "@/store";
+import { currencyFormat, sleep } from "@/utils";
+import clsx from "clsx";
+
+export const PlaceOrder = () => {
+  const [loaded, setLoaded] = useState(false);
+  const [isPlacingOrder, setIsPlacingOrder] = useState(false);
+
+  const address = useAddressStore((state) => state.address);
+
+  const itemsInCart = useCartStore((state) => state.getTotalItems());
+  const subTotal = useCartStore((state) => state.getSubTotal());
+  const tax = useCartStore((state) => state.getTax());
+  const total = useCartStore((state) => state.getTotal());
+  const cart = useCartStore(state => state.cart);
+
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  if (!loaded) {
+    return <p>Cargando....</p>
+  }
+
+  const onPlaceOrder = async () => {
+    setIsPlacingOrder(true);
+
+    const productsToOrder = cart.map(product => ({
+      productId: product.id,
+      quantity: product.quantity,
+      size: product.size,
+    }));
+
+    console.log({ address, productsToOrder })
+
+
+    //await sleep(2);
+    setIsPlacingOrder(false);
+  }
+
+  return (
+    <div className="bg-white rounded-xl shadow-xl p-7">
+
+      <h2 className="text-2xl mb-2">Dirección de entrega</h2>
+      <div className="mb-10">
+        <p className="text-xl">
+          {address.firstName} {address.lastName}
+        </p>
+        <p>{address.address}</p>
+        <p>{address.address2}</p>
+        <p>{address.postalCode}</p>
+        <p>
+          {address.city}, {address.country}
+        </p>
+        <p>{address.phone}</p>
+      </div>
+
+      {/* Divider */}
+      <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+
+
+      <h2 className="text-2xl mb-2">Resumen de orden</h2>
+
+      <div className="grid grid-cols-2">
+        <span>No. Productos</span>
+        <span className="text-right">
+          {itemsInCart === 1 ? "1 artículo" : `${itemsInCart} artículos`}
+        </span>
+
+        <span>Subtotal</span>
+        <span className="text-right">{currencyFormat(subTotal)}</span>
+
+        <span>Impuestos (15%)</span>
+        <span className="text-right">{currencyFormat(tax)}</span>
+
+        <span className="mt-5 text-2xl">Total:</span>
+        <span className="mt-5 text-2xl text-right">{currencyFormat(total)}</span>
+      </div>
+
+      <div className="mt-5 mb-2 w-full">
+
+        <p className="mb-5">
+          {/* Disclaimer */}
+          <span className="text-xs">
+            Al hacer clic en `&quot;`Colocar orden`&quot;`, aceptas nuestros <a href="#" className="underline">términos y condiciones</a> y <a href="#" className="underline">política de privacidad</a>
+          </span>
+        </p>
+
+        {/* <p className="text-red-500">{errorMessage}</p> */}
+
+
+        <button
+          // href="/orders/123"
+          onClick={onPlaceOrder}
+          className={
+            clsx({
+              'btn-primary': !isPlacingOrder,
+              'btn-disabled': isPlacingOrder
+            })
+          }
+        >
+          Colocar orden
+        </button>
+
+      </div>
+    </div>
+  )
+}
