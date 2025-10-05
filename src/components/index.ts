36536158ebf9aae810af6699ea90e785dd2b4d64
vista-juan-constantine <juan.constantine@vistadash.com>
2025-10-05 18:51:24 -0500

    Added the products list

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
 
 export * from './product/quantity-selector/QuantitySelector';
+export * from './product/product-image/ProductImage';
 export * from './product/size-selector/SizeSelector';
 export * from './product/slideshow/ProductMobileSlideshow';
 export * from './product/slideshow/ProductSlideshow';
